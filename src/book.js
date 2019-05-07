38ec8c04f05fc78979e272a22fdfba397b575e92
Jitřenka <jnn@post.cz>
2019-05-07 19:23:26 +0200

    Initial commit

---
+++ src/book.js
@@ -0,0 +1,29 @@
+/*
+  Třída pro jednu knihu v knihovně.
+
+  Vlastnosti:
+  - author: autor knihy
+  - title: název khiny
+  - year: rok vydání
+  - image: obrázek knihy
+  - isRead: boolean (true/false) zda máme knihu přečtenou
+
+  Metody:
+  - read(): označí knihu jako přečtenou
+*/
+export default class Book {
+
+  constructor(author, title, year, image) {
+    this.author = author;
+    this.title = title;
+    this.year = year;
+    this.image = image;
+    this.isRead = false;
+  }
+
+  read() {
+    this.isRead = true;
+    console.log(`Super, přečetl jsi knihu ${this.title}.`);
+  }
+
+}
---
+++ src/library.js
@@ -0,0 +1,85 @@
+/*
+  Třída pro knihovnu.
+
+  Vlastnosti:
+  - booklist: pole knih (objektů třídy Book)
+  - lastBook: poslední přečtená kniha (objekt třídy Book)
+  - currentBook: aktuálně čtená kniha (objekt třídy Book)
+  - nextBook: příští kniha k přečtení (objekt třídy Book)
+  - unreadBooks: počet nepřečtených knih
+
+  Metody:
+  - addBook(book): přidá knihu do knihovny
+  - listAllBooks(): výpis všech knih v knihovně
+  - startReadingNextBook(): začne čtení další knihy (tj. příští knihu přesune do aktuálně čtené knihy a do příští knihy vloží první nepřečtenou knihu v seznamu)
+*/
+export default class Library {
+
+  constructor() {
+    this.bookList = [];
+    this.lastBook = null;
+    this.currentBook = null;
+    this.nextBook = null;
+    this.unreadBooks = 0;
+  }
+
+  /* přidat knihu do knihovny */
+  addBook(book) {
+    this.bookList.push(book);
+
+    if (!book.isRead) {
+      this.unreadBooks++;
+
+      if (this.nextBook === null) {
+        this.nextBook = book;
+      }
+    }
+  }
+
+  /* výpis všech knih v knihovně */
+  listAllBooks() {
+    console.table(this.bookList);
+   // console.log(this.currentBook);
+    //console.log(this.nextBook);
+  }
+
+  /* začít číst další knihu */
+  startReadingNextBook() {
+    if (this.nextBook !== null) {
+      // příští knihu ke čtení dáme do aktuálně čtené knihy
+      this.currentBook = this.nextBook;
+      this.nextBook = null;
+
+      // do příští knihy ke čtení dáme první nepřečtenou knihu v seznamu
+      for (let book of this.bookList) {
+        if (!book.isRead && book !== this.currentBook) {
+          this.nextBook = book;
+          break;
+        }
+      }
+    }
+  }
+
+  finishCurrentBook() {
+    if (this.currentBook !== null) {
+      this.currentBook.read();
+      this.lastBook = this.currentBook;
+      this.currentBook = null;
+      this.unreadBooks = this.unreadBooks - 1;
+
+      console.log(this.currentBook);
+      console.log(this.lastBook);
+      console.log(this.unreadBooks);
+      console.log(this.nextBook);
+    }
+  }
+
+  listUnreadBooks() {
+    let test = this.bookList.filter(chooseBook);
+    console.log(test);
+
+    function chooseBook(book) {
+      return book.isRead !== true;
+      }
+  }
+}
